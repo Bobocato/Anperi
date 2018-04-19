88e1eb11644cc35abaddb1edd1880bed8b8da947
JannesP <jannespeters.privat@gmail.com>
2018-04-19 17:13:43 +0200

    More bugfixing.

--- src/JJA.Anperi.Host/JJA.Anperi.Host/Utility/HostUtil.cs
+++ src/JJA.Anperi.Host/JJA.Anperi.Host/Utility/HostUtil.cs
@@ -33,5 +33,31 @@ namespace JJA.Anperi.Host.Utility
             return result;
         }
 
+        /// <summary>
+        /// TryGetValue with typecheck for dynamic dictionary.
+        /// </summary>
+        /// <returns>if the key existed and the dynamic was of the given type</returns>
+        internal static bool TryGetValue<TK, T>(this Dictionary<TK, dynamic> dict, TK key, out T val)
+        {
+            bool result = false;
+            if (dict.TryGetValue(key, out dynamic dyn))
+            {
+                try
+                {
+                    val = (T)dyn;
+                    result = true;
+                }
+                catch (RuntimeBinderException)
+                {
+                    val = default(T);
+                }
+            }
+            else
+            {
+                val = default(T);
+            }
+            return result;
+        }
+
     }
 }
