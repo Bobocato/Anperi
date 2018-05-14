98413ee6dcd38376c2ac3db1ae961319f52f97f0
JannesP <jannespeters.privat@gmail.com>
2018-05-14 18:20:38 +0200

    Pairing codes now time out after 30-60 minutes. Also updated ASP.net/MySql deps.

---
+++ src/cs/Server/JJA.Anperi.Server/AnperiDbCleanup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JJA.Anperi.Server.Model;
+using JJA.Anperi.Server.Utility;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JJA.Anperi.Server
+{
+    public class AnperiDbCleanup : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly ILogger<AnperiDbCleanup> _logger;
+
+        public AnperiDbCleanup(IServiceScopeFactory serviceScopeFactory, ILogger<AnperiDbCleanup> logger)
+        {
+            _logger = logger;
+            _scopeFactory = serviceScopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (IServiceScope scope = _scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AnperiDbContext>();
+                while (!stoppingToken.IsCancellationRequested)
+                {
+
+                    _logger.LogInformation("Starting db cleanup.");
+                    db.ActivePairingCodes.RemoveRange(db.ActivePairingCodes.Where(apc =>
+                        (DateTime.Now - apc.Created) > TimeSpan.FromMinutes(30)));
+                    int res = await db.SaveChangesAsync(stoppingToken);
+                    _logger.LogInformation("Removed {0} entries.", res);
+                    await Task.Delay(TimeSpan.FromMinutes(30), stoppingToken);
+                }
+            }
+        }
+    }
+}
---
+++ src/cs/Server/JJA.Anperi.Server/Utility/BackgroundService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace JJA.Anperi.Server.Utility
+{
+    // Copyright (c) .NET Foundation. Licensed under the Apache License, Version 2.0.
+    // as stated https://blogs.msdn.microsoft.com/cesardelatorre/2017/11/18/implementing-background-tasks-in-microservices-with-ihostedservice-and-the-backgroundservice-class-net-core-2-x/
+    // this class will be part of ASP.net Core 2.1 when it hits release status
+    /// <summary>
+    /// Base class for implementing a long running <see cref="IHostedService"/>.
+    /// </summary>
+    public abstract class BackgroundService : IHostedService, IDisposable
+    {
+        private Task _executingTask;
+        private readonly CancellationTokenSource _stoppingCts =
+            new CancellationTokenSource();
+
+        protected abstract Task ExecuteAsync(CancellationToken stoppingToken);
+
+        public virtual Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Store the task we're executing
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+
+            // If the task is completed then return it,
+            // this will bubble cancellation and failure to the caller
+            if (_executingTask.IsCompleted)
+            {
+                return _executingTask;
+            }
+
+            // Otherwise it's running
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task StopAsync(CancellationToken cancellationToken)
+        {
+            // Stop called without start
+            if (_executingTask == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Signal cancellation to the executing method
+                _stoppingCts.Cancel();
+            }
+            finally
+            {
+                // Wait until the task completes or the stop token triggers
+                await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite,
+                    cancellationToken));
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            _stoppingCts.Cancel();
+        }
+    }
+}
