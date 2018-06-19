fde965c4ddf1fd921336d31716c490325076cf37
JannesP <jannespeters.privat@gmail.com>
2018-06-19 11:28:53 +0200

    Add spacer to C# api. Fix #50

---
+++ src/cs/Library/Elements/Spacer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JJA.Anperi.Lib.Elements
+{
+    public class Spacer : Element
+    {
+        public override string type => "spacer";
+    }
+}
